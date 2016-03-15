ad557e3e92457710d5c39dfe8b13baef802e0636
Sergey Sergeev <sergey.sergeev.oneclick@gmail.com>
2016-03-15 21:58:42 +0200

    added runloop

--- public_html/src/render/gl_context.js
+++ public_html/src/render/gl_context.js
@@ -4,23 +4,45 @@
  * and open the template in the editor.
  */
 
-var gl_context = (function (window) 
+var gl_context = (function() 
 {
-    var gl_context_instance = null;
+    var m_instance = null;
  
     function init()
     {
-        gl_context_instance = window.webgl_context;
+        var canvas = document.getElementById("gl_canvas");
+        try 
+        {
+            m_instance = canvas.getContext("experimental-webgl");
+            m_instance.viewportWidth = canvas.width;
+            m_instance.viewportHeight = canvas.height;
+        } 
+        catch (exception) 
+        {
+                    
+        }
+        if (!m_instance) 
+        {
+            alert("could not initialise gl context");
+        }
+        else
+        {
+            m_instance.clearColor(0.0, 0.0, 0.0, 1.0);
+            m_instance.enable(m_instance.DEPTH_TEST);
+                    
+            m_instance.viewport(0, 0, m_instance.viewportWidth, m_instance.viewportHeight);
+            m_instance.clear(m_instance.COLOR_BUFFER_BIT | m_instance.DEPTH_BUFFER_BIT);
+        }
     }
  
     return {
         get_instance: function() 
         {
-            if (!gl_context_instance)
+            if (!m_instance)
             {
-                gl_context_instance = init();
+                m_instance = init();
             }
-            return gl_context_instance;
+            return m_instance;
         }
     };
-})(window);
+})();
